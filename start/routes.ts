0582678b6357eb5f07be47012ee119554537368b
krrrmrl <diajeng1701@gmail.com>
2025-11-28 20:33:09 +0700

    base frontend #1

--- start/routes.ts
+++ start/routes.ts
@@ -9,4 +9,20 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+/*router.on('/').render('pages/home')*/
+router.on('/').render('pages/intro')
+router.on('/fanworks').render('pages/homepage/fanworks')
+router.on('/wiki').render('pages/homepage/wiki')
+router.on('/forum').render('pages/homepage/forum')
+router.on('/chats').render('pages/chats')
+router.on('/profile').render('pages/profile')
+
+router.get('/search', async ({ request, view }) => {
+  const query = request.input('q')
+  const activeTab = request.input('tab') || 'fanworks'
+
+  return view.render('pages/search', {
+    query,
+    activeTab,
+  })
+})
